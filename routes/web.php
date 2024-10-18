b6b2f2634fae6b496cfad62987cbe08f0fcbbf4d
Patricia <ppatricia@ipvc.pt>
2024-10-18 23:42:32 +0100

    Utilização de um Starter Kit Breeze com Blade

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,20 @@
 <?php
 
-use App\Http\Controllers\ReclamacaoController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CondominioController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('condominio', CondominioController::class);
-Route::resource('reclamacao', ReclamacaoController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
+require __DIR__.'/auth.php';
